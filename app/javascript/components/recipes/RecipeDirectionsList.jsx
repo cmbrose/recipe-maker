f51a8e94b275adf2281b2312c0c2a6c848d961b4
Caleb Brose <5447118+cmbrose@users.noreply.github.com>
2021-08-20 17:06:41 +0000

    Add reorder and delete to directions

--- app/javascript/components/recipes/RecipeDirectionsList.jsx
+++ app/javascript/components/recipes/RecipeDirectionsList.jsx
@@ -1,25 +1,69 @@
 import React from "react";
+import ReactDragListView from "react-drag-listview";
 
 import AutoHeightTextArea from "../AutoHeightTextArea";
 
 const RecipeDirectionsList = ({ directions, editable, onUpdate, ...other }) => {
   var items = directions.map((direction, idx) => (
     <li key={idx}>
-      {renderItem(direction, editable, (value) => { directions[idx] = value; onUpdate(directions); })}
+      {renderItem(
+        direction,
+        editable,
+        (value) => {
+          directions[idx] = value; onUpdate(directions);
+        },
+        () => {
+          directions.splice(idx, 1);
+          onUpdate(directions);
+        }
+      )}
     </li>
   ));
 
-  return <ol>{items}</ol>;
+  const dragProps = {
+    onDragEnd(fromIndex, toIndex) {
+      const item = directions.splice(fromIndex, 1)[0];
+      directions.splice(toIndex, 0, item);
+      onUpdate(directions);
+    },
+    nodeSelector: 'li',
+    handleSelector: '.drag-point'
+  };
+
+  return (
+    <ReactDragListView {...dragProps}>
+      <ol>{items}</ol>
+    </ReactDragListView>
+  );
 };
 
-const renderItem = (item, editable, onUpdate) => {
+const renderItem = (item, editable, onUpdate, onDelete) => {
   if (editable) {
     return (
-      <AutoHeightTextArea
-        classes={["recipe-direction-edit-area"]}
-        value={item}
-        onUpdate={onUpdate}
-      />
+      <div className="d-flex">
+        <div className="drag-point">
+          <button type="button" class="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-grow-1">
+          <AutoHeightTextArea
+            classes={["recipe-direction-edit-area"]}
+            value={item}
+            onUpdate={onUpdate}
+          />
+        </div>
+        <div>
+          <button type="button" class="btn" onClick={onDelete}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+            </svg>
+          </button>
+        </div>
+      </div>
     );
   } else {
     return <>{item}</>;
--- app/javascript/components/recipes/RecipeEditor.jsx
+++ app/javascript/components/recipes/RecipeEditor.jsx
@@ -1,4 +1,3 @@
-import { Button } from "bootstrap";
 import React from "react";
 import RecipeDetails from "./RecipeDetails";
 

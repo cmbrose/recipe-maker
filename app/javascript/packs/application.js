ef2bf41918122e9f29280ff22d41abb1ebd34c0b
Caleb Brose <5447118+cmbrose@users.noreply.github.com>
2023-05-08 00:25:49 +0000

    Fix buttons not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require('jquery')
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import "bootstrap";
 import "../stylesheets/application";// Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
